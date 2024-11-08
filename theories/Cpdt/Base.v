14fa3fabf3da2b849689867ef297b69a8b62a535
Henry Till <henrytill@gmail.com>
2024-11-08 02:02:15 -0800

    Cpdt/Base: tweak

--- theories/Cpdt/Base.v
+++ theories/Cpdt/Base.v
@@ -1,9 +1,10 @@
-Require Import List.
+From Coq Require Import List.
 Import List.ListNotations.
 
 Lemma app_assoc_reverse {A : Type} (l m n : list A) :
   (l ++ m) ++ n = l ++ m ++ n.
 Proof. symmetry. apply app_assoc. Qed.
 
-Theorem app_nil_end {A : Type} (l : list A) : l = l ++ [].
+Theorem app_nil_end {A : Type} (l : list A) :
+  l = l ++ [].
 Proof. symmetry; apply app_nil_r. Qed.
