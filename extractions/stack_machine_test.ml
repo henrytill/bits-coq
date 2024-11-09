8c73189bcb75813d0e5ce243b5091802e3cc1f96
Henry Till <henrytill@gmail.com>
2024-11-08 16:51:29 -0800

    Stack_machine_test: expand

--- extractions/stack_machine_test.ml
+++ extractions/stack_machine_test.ml
@@ -36,27 +36,87 @@ module Tests = struct
   let testable_nat = Alcotest.testable pp_nat equal_nat
   let testable_prog = Alcotest.testable Prog.pp Prog.equal
 
+  (* Example values *)
+  let nconst = Exp.NConst (nat_of_int 42)
+  let bconst = Exp.BConst false
+
+  let plus =
+    Exp.Binop
+      (Ty.Nat, Ty.Nat, Ty.Nat, Binop.Plus, Exp.NConst (nat_of_int 2), Exp.NConst (nat_of_int 2))
+
+  let nested = Exp.Binop (Ty.Nat, Ty.Nat, Ty.Nat, Binop.Times, plus, Exp.NConst (nat_of_int 7))
+
+  let nested_eq =
+    Exp.Binop (Ty.Nat, Ty.Nat, Ty.Bool, Binop.Eq Ty.Nat, plus, Exp.NConst (nat_of_int 7))
+
+  let nested_lt = Exp.Binop (Ty.Nat, Ty.Nat, Ty.Bool, Binop.Lt, plus, Exp.NConst (nat_of_int 7))
+
   let denote_nconst () =
     let expected = nat_of_int 42 in
-    let nconst = Exp.NConst expected in
     let actual = Exp.denote Ty.Nat nconst |> Obj.obj in
     Alcotest.(check testable_nat) "same nat" expected actual
 
+  let denote_bconst () =
+    let expected = false in
+    let actual = Exp.denote Ty.Nat bconst |> Obj.obj in
+    Alcotest.(check bool) "same nat" expected actual
+
+  let denote_plus () =
+    let expected = nat_of_int 4 in
+    let actual = Exp.denote Ty.Nat plus |> Obj.obj in
+    Alcotest.(check testable_nat) "same nat" expected actual
+
+  let denote_nested () =
+    let expected = nat_of_int 28 in
+    let actual = Exp.denote Ty.Nat nested |> Obj.obj in
+    Alcotest.(check testable_nat) "same nat" expected actual
+
+  let denote_nested_eq () =
+    let expected = false in
+    let actual = Exp.denote Ty.Nat nested_eq |> Obj.obj in
+    Alcotest.(check bool) "same bool" expected actual
+
+  let denote_nested_lt () =
+    let expected = true in
+    let actual = Exp.denote Ty.Nat nested_lt |> Obj.obj in
+    Alcotest.(check bool) "same bool" expected actual
+
   let compile_nconst () =
-    let input = nat_of_int 42 in
     let expected =
-      Prog.Cons ([], [ Ty.Nat ], [ Ty.Nat ], Instr.NConst ([], input), Prog.Nil [ Ty.Nat ])
+      Prog.Cons ([], [ Ty.Nat ], [ Ty.Nat ], Instr.NConst ([], nat_of_int 42), Prog.Nil [ Ty.Nat ])
     in
-    let nconst = Exp.NConst input in
     let actual = Exp.compile Ty.Nat nconst [] in
     Alcotest.(check testable_prog) "same prog" expected actual
 
   let denote_compile_nconst () =
-    let input = nat_of_int 42 in
-    let expected = (input, ()) in
-    let nconst = Exp.NConst input in
+    let expected = (nat_of_int 42, ()) in
     let actual = Prog.denote [] [] (Exp.compile Ty.Nat nconst []) (Obj.repr ()) |> Obj.obj in
     Alcotest.(check (pair testable_nat unit)) "same pair" expected actual
+
+  let denote_compile_bconst () =
+    let expected = (false, ()) in
+    let actual = Prog.denote [] [] (Exp.compile Ty.Bool bconst []) (Obj.repr ()) |> Obj.obj in
+    Alcotest.(check (pair bool unit)) "same pair" expected actual
+
+  let denote_compile_plus () =
+    let expected = (nat_of_int 4, ()) in
+    let actual = Prog.denote [] [] (Exp.compile Ty.Nat plus []) (Obj.repr ()) |> Obj.obj in
+    Alcotest.(check (pair testable_nat unit)) "same pair" expected actual
+
+  let denote_compile_nested () =
+    let expected = (nat_of_int 28, ()) in
+    let actual = Prog.denote [] [] (Exp.compile Ty.Nat nested []) (Obj.repr ()) |> Obj.obj in
+    Alcotest.(check (pair testable_nat unit)) "same pair" expected actual
+
+  let denote_compile_nested_eq () =
+    let expected = (false, ()) in
+    let actual = Prog.denote [] [] (Exp.compile Ty.Bool nested_eq []) (Obj.repr ()) |> Obj.obj in
+    Alcotest.(check (pair bool unit)) "same pair" expected actual
+
+  let denote_compile_nested_lt () =
+    let expected = (true, ()) in
+    let actual = Prog.denote [] [] (Exp.compile Ty.Bool nested_lt []) (Obj.repr ()) |> Obj.obj in
+    Alcotest.(check (pair bool unit)) "same pair" expected actual
 end
 
 let () =
@@ -66,7 +126,17 @@ let () =
       ( "Exp",
         [
           test_case "denote_nconst" `Quick Tests.denote_nconst;
+          test_case "denote_bconst" `Quick Tests.denote_bconst;
+          test_case "denote_plus" `Quick Tests.denote_plus;
+          test_case "denote_nested" `Quick Tests.denote_nested;
+          test_case "denote_nested_eq" `Quick Tests.denote_nested_eq;
+          test_case "denote_nested_lt" `Quick Tests.denote_nested_lt;
           test_case "compile_nconst" `Quick Tests.compile_nconst;
           test_case "denote_compile_nconst" `Quick Tests.denote_compile_nconst;
+          test_case "denote_compile_bconst" `Quick Tests.denote_compile_bconst;
+          test_case "denote_compile_plus" `Quick Tests.denote_compile_plus;
+          test_case "denote_compile_nested" `Quick Tests.denote_compile_nested;
+          test_case "denote_compile_nested_eq" `Quick Tests.denote_compile_nested_eq;
+          test_case "denote_compile_nested_lt" `Quick Tests.denote_compile_nested_lt;
         ] );
     ]
